30bb2ec01cf42b4cc9b677e16f697c1c5f0a016d
lazuardirr <lazuardi.rea@gmail.com>
2015-10-24 00:19:39 +0700

    Implement flash message html & javascript code

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,18 +5,23 @@
     <title>Dailylarva</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{url('css/dailylarva.css')}}">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
 </head>
 <body>
 <div class="container">
     <div class="row">
         <div class="col-md-3">Sidebar</div>
         <div class="col-md-9">
+            @include('flash::message')
+
             @yield('content')
         </div>
     </div>
 </div>
+<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script>
+    $('div.alert').not('.alert-important').delay(3000).slideUp(300);
+</script>
 <script>
     jQuery(document).ready(function ($) {
         $(".clickable-row").click(function () {
