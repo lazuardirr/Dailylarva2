6c2864b3eeaede1f2c004621589f3e067b45d9a2
lazuardirr <lazuardi.rea@gmail.com>
2015-10-24 00:11:18 +0700

    Add Laracasts/Flash service provider & facade

--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
 
     ],
 
@@ -196,6 +197,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Illuminate\Html\FormFacade::class,
         'Html' => Illuminate\Html\HtmlFacade::class,
+        'Flash' => Laracasts\Flash\Flash::class,
 
     ],
 
