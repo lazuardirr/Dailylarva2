5ce3fa6d2fb171c55f4e185ec94ddee6c4817976
lazuardirr <lazuardi.rea@gmail.com>
2015-10-23 07:04:20 +0700

    New blade.php file for edit Agent form

---
+++ resources/views/pages/agents/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('dashboard')
+
+@section('content')
+    <div class="page-content-wrapper">
+        <div class="page-content">
+            <!-- BEGIN PAGE HEADER-->
+            <h3 class="page-title">
+                Edit: {{ $agent->email }}
+                <small> please input new agent and more</small>
+            </h3>
+            <!-- END PAGE HEADER-->
+            <!-- BEGIN PAGE CONTENT-->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="portlet box blue">
+                        <div class="portlet-title">
+                            <div class="caption">
+                                <i class="fa fa-gift"></i> Add New Agent
+                            </div>
+                        </div>
+                        <div class="portlet-body form">
+                            {!! Form::model($agent, ['method' => 'PATCH', 'action' => ['AgentsController@update', $agent->id]]) !!}
+                            @include('pages.agents.form', ['submitButtonText' => 'Edit Agent'])
+                            {!! Form::close() !!}
+
+                            @include('errors.list')
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- END PAGE CONTENT-->
+        </div>
+    </div>
+@stop
