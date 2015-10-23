5bcc88fba08f30bac69f24a16ee6e5188867b79d
lazuardirr <lazuardi.rea@gmail.com>
2015-10-24 00:16:58 +0700

    Add new ContentGenerator class

---
+++ app/lib/ContentGenerator.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace App\lib;
+
+class ContentGenerator
+{
+    protected $landing_page;
+    protected $data;
+    protected $codes = array();
+
+
+    /**
+     * Getter method for $this->data
+     *
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+
+    /**
+     * Constructor
+     *
+     * @param $movie_id
+     */
+    public function __construct($movie_id)
+    {
+        $this->data = json_decode(file_get_contents('http://cdn.asghia.com/index.php?movie=' . $movie_id));
+        $this->landing_page = 'http://icoolmovie.com/hd-movie/' . $this->getData()->id .
+            '-' .
+            $this->play_slug($this->getData()->title) .
+            '.html';
+        $this->codes = array(
+            'movie_title' => $this->getData()->title,
+            'movie_overview' => $this->getData()->overview,
+            'short_url' => $this->tinyurl_short_url($this->landing_page)
+        );
+    }
+
+    /**
+     * Check if json data exists
+     *
+     * @return bool
+     */
+    private function isDataExists()
+    {
+        if ($this->getData()->id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Check if thumbnail file exists
+     *
+     * @return bool
+     */
+    private function isThumbnailExists()
+    {
+        if (count($this->getData()->images->backdrops) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Generate Description field from JSON and content_template.
+     *
+     * @return bool
+     */
+    public function getDescription()
+    {
+        if ($this->isDataExists()) {
+            dd(view('lib.ContentGenerator.content_template', $this->codes)->render());
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get Thumbnail Url from JSON data.
+     *
+     * @return bool|string
+     */
+    public function getThumbnail()
+    {
+        if ($this->isDataExists()) {
+            if ($this->isThumbnailExists()) {
+                return 'http://image.tmdb.org/t/p/w1280' .
+                $this->getData()
+                    ->images
+                    ->backdrops[rand(0, count($this->getData()->images->backdrops) - 1)]
+                    ->file_path;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Slug method.
+     *
+     * @param $str
+     * @param array $options
+     * @return string
+     */
+    private function play_slug($str, $options = array())
+    {
+        $str = mb_convert_encoding((string)$str, 'UTF-8', mb_list_encodings());
+        $defaults = array(
+            'delimiter' => '-',
+            'limit' => null,
+            'lowercase' => true,
+            'replacements' => array(),
+            'transliterate' => false,
+        );
+        $options = array_merge($defaults, $options);
+        $char_map = array(
+            // Latin
+            'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'AE', 'Ç' => 'C',
+            'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I',
+            'Ð' => 'D', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', '?' => 'O',
+            'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', '?' => 'U', 'Ý' => 'Y', 'Þ' => 'TH',
+            'ß' => 'ss',
+            'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'ae', 'ç' => 'c',
+            'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+            'ð' => 'd', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', '?' => 'o',
+            'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', '?' => 'u', 'ý' => 'y', 'þ' => 'th',
+            'ÿ' => 'y',
+
+            // Latin symbols
+            '©' => '(c)',
+
+            // Greek
+            '?' => 'A', '?' => 'B', '?' => 'G', '?' => 'D', '?' => 'E', '?' => 'Z', '?' => 'H', '?' => '8',
+            '?' => 'I', '?' => 'K', '?' => 'L', '?' => 'M', '?' => 'N', '?' => '3', '?' => 'O', '?' => 'P',
+            '?' => 'R', '?' => 'S', '?' => 'T', '?' => 'Y', '?' => 'F', '?' => 'X', '?' => 'PS', '?' => 'W',
+            '?' => 'A', '?' => 'E', '?' => 'I', '?' => 'O', '?' => 'Y', '?' => 'H', '?' => 'W', '?' => 'I',
+            '?' => 'Y',
+            '?' => 'a', '?' => 'b', '?' => 'g', '?' => 'd', '?' => 'e', '?' => 'z', '?' => 'h', '?' => '8',
+            '?' => 'i', '?' => 'k', '?' => 'l', '?' => 'm', '?' => 'n', '?' => '3', '?' => 'o', '?' => 'p',
+            '?' => 'r', '?' => 's', '?' => 't', '?' => 'y', '?' => 'f', '?' => 'x', '?' => 'ps', '?' => 'w',
+            '?' => 'a', '?' => 'e', '?' => 'i', '?' => 'o', '?' => 'y', '?' => 'h', '?' => 'w', '?' => 's',
+            '?' => 'i', '?' => 'y', '?' => 'y', '?' => 'i',
+
+            // Turkish
+            '?' => 'S', '?' => 'I', 'Ç' => 'C', 'Ü' => 'U', 'Ö' => 'O', '?' => 'G',
+            '?' => 's', '?' => 'i', 'ç' => 'c', 'ü' => 'u', 'ö' => 'o', '?' => 'g',
+
+            // Russian
+            '?' => 'A', '?' => 'B', '?' => 'V', '?' => 'G', '?' => 'D', '?' => 'E', '?' => 'Yo', '?' => 'Zh',
+            '?' => 'Z', '?' => 'I', '?' => 'J', '?' => 'K', '?' => 'L', '?' => 'M', '?' => 'N', '?' => 'O',
+            '?' => 'P', '?' => 'R', '?' => 'S', '?' => 'T', '?' => 'U', '?' => 'F', '?' => 'H', '?' => 'C',
+            '?' => 'Ch', '?' => 'Sh', '?' => 'Sh', '?' => '', '?' => 'Y', '?' => '', '?' => 'E', '?' => 'Yu',
+            '?' => 'Ya',
+            '?' => 'a', '?' => 'b', '?' => 'v', '?' => 'g', '?' => 'd', '?' => 'e', '?' => 'yo', '?' => 'zh',
+            '?' => 'z', '?' => 'i', '?' => 'j', '?' => 'k', '?' => 'l', '?' => 'm', '?' => 'n', '?' => 'o',
+            '?' => 'p', '?' => 'r', '?' => 's', '?' => 't', '?' => 'u', '?' => 'f', '?' => 'h', '?' => 'c',
+            '?' => 'ch', '?' => 'sh', '?' => 'sh', '?' => '', '?' => 'y', '?' => '', '?' => 'e', '?' => 'yu',
+            '?' => 'ya',
+
+            // Ukrainian
+            '?' => 'Ye', '?' => 'I', '?' => 'Yi', '?' => 'G',
+            '?' => 'ye', '?' => 'i', '?' => 'yi', '?' => 'g',
+
+            // Czech
+            '?' => 'C', '?' => 'D', '?' => 'E', '?' => 'N', '?' => 'R', 'Š' => 'S', '?' => 'T', '?' => 'U',
+            'Ž' => 'Z',
+            '?' => 'c', '?' => 'd', '?' => 'e', '?' => 'n', '?' => 'r', 'š' => 's', '?' => 't', '?' => 'u',
+            'ž' => 'z',
+
+            // Polish
+            '?' => 'A', '?' => 'C', '?' => 'e', '?' => 'L', '?' => 'N', 'Ó' => 'o', '?' => 'S', '?' => 'Z',
+            '?' => 'Z',
+            '?' => 'a', '?' => 'c', '?' => 'e', '?' => 'l', '?' => 'n', 'ó' => 'o', '?' => 's', '?' => 'z',
+            '?' => 'z',
+
+            // Latvian
+            '?' => 'A', '?' => 'C', '?' => 'E', '?' => 'G', '?' => 'i', '?' => 'k', '?' => 'L', '?' => 'N',
+            'Š' => 'S', '?' => 'u', 'Ž' => 'Z',
+            '?' => 'a', '?' => 'c', '?' => 'e', '?' => 'g', '?' => 'i', '?' => 'k', '?' => 'l', '?' => 'n',
+            'š' => 's', '?' => 'u', 'ž' => 'z'
+        );
+
+        // Make custom replacements
+        $str = preg_replace(array_keys($options['replacements']), $options['replacements'], $str);
+
+        // Transliterate characters to ASCII
+        if ($options['transliterate']) {
+            $str = str_replace(array_keys($char_map), $char_map, $str);
+        }
+
+        // Replace non-alphanumeric characters with our delimiter
+        $str = preg_replace('/[^\p{L}\p{Nd}]+/u', $options['delimiter'], $str);
+
+        // Remove duplicate delimiters
+        $str = preg_replace('/(' . preg_quote($options['delimiter'], '/') . '){2,}/', '$1', $str);
+
+        // Truncate slug to max. characters
+        $str = mb_substr($str, 0, ($options['limit'] ? $options['limit'] : mb_strlen($str, 'UTF-8')), 'UTF-8');
+
+        // Remove delimiter from ends
+        $str = trim($str, $options['delimiter']);
+
+        return $options['lowercase'] ? mb_strtolower($str, 'UTF-8') : $str;
+    }
+
+    /**
+     * Tinyurl curl method.
+     *
+     * @param $url
+     * @return mixed
+     */
+    private function tinyurl_short_url($url)
+    {
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch, CURLOPT_URL, 'http://tinyurl.com/api-create.php?url=' . $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $content = curl_exec($ch);
+        curl_close($ch);
+        return $content;
+    }
+}
