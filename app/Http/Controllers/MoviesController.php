c8b1ec612897b9696168da94c98a1b3ebaf08341
lazuardirr <lazuardi.rea@gmail.com>
2015-10-24 08:17:41 +0700

    Add a new method getMovie() which handles json request Move ContentGeneration from Model to Controller Minor content changes

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -4,30 +4,61 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use App\Http\Requests\MovieRequest;
+use App\lib\ContentGenerator;
 use App\Movie;
 
 class MoviesController extends Controller
 {
+    /**
+     * Show all movies
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
-        $movies = Movie::all();
+        $movies = Movie::oldest()->get();//all();
         return view('pages.movies.view', compact('movies'));
     }
 
+    /**
+     * Show movies creation form page
+     *
+     * @return \Illuminate\View\View
+     */
     public function create()
     {
         return view('pages.movies.create');
     }
 
+    /**
+     * Save new Movie.
+     *
+     * @param MovieRequest $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function store(MovieRequest $request)
     {
+        $content = new ContentGenerator($request->get('id'));
         $movie = new Movie($request->all());
-        $movie->setThumbnail();
-        $movie->setDescription();
+        $movie->setThumbnail($content);
+        $movie->setDescription($content);
         $movie->save();
 
         flash('New Agent have been created.');
 
         return redirect('movies');
+    }
+
+    /**
+     * Handle AJAX request of movie id search tools.
+     *
+     * @param $title
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getMovie($title)
+    {
+        $data = json_decode(file_get_contents('http://cdn.asghia.com/index.php?query=' . $title));
+
+        return response()->json($data);
     }
 }
