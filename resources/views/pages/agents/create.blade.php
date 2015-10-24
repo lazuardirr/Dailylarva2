e6397a462e0e87f667c7782d5fb878ec8f8bff5a
lazuardirr <lazuardi.rea@gmail.com>
2015-10-24 08:02:21 +0700

    Minor content changes

--- resources/views/pages/agents/create.blade.php
+++ resources/views/pages/agents/create.blade.php
@@ -5,22 +5,21 @@
         <div class="page-content">
             <!-- BEGIN PAGE HEADER-->
             <h3 class="page-title">
-                Input New Agent
-                <small> please input new agent and more</small>
+                Create a new Agent
             </h3>
             <!-- END PAGE HEADER-->
             <!-- BEGIN PAGE CONTENT-->
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-8">
                     <div class="portlet box blue">
                         <div class="portlet-title">
                             <div class="caption">
-                                <i class="fa fa-gift"></i> Add New Agent
+                                <i class="fa fa-gift"></i>
                             </div>
                         </div>
                         <div class="portlet-body form">
                             {!! Form::open(['url' => 'agents']) !!}
-                            @include('pages.partials.form', ['pages'=> 'agents', 'submitButtonText' => 'Add Agent'])
+                            @include('pages.partials.form', ['pages'=> 'agents', 'submitButtonText' => 'Create Agent'])
                             {!! Form::close() !!}
 
                             @include('errors.list')
