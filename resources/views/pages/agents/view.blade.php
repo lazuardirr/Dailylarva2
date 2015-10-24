99744d68150bae96ec09aba7ba6f15550563c612
lazuardirr <lazuardi.rea@gmail.com>
2015-10-24 08:20:02 +0700

    Minor layout adjustment

--- resources/views/pages/agents/view.blade.php
+++ resources/views/pages/agents/view.blade.php
@@ -1,7 +1,7 @@
 @extends('dashboard')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <!-- BEGIN PAGE HEADER-->
         <h3 class="page-title">
             View Agent Database
@@ -10,7 +10,7 @@
         <!-- END PAGE HEADER-->
         <!-- BEGIN PAGE CONTENT-->
         <div class="row">
-            <div class="col-md-9">
+            <div class="col-md-12">
                 <!-- BEGIN EXAMPLE TABLE PORTLET-->
                 <div class="portlet box grey-cascade">
                     <div class="portlet-title">
