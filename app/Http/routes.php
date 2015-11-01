56d355ad28bb6cf02213569ba92642164b395cb4
lazuardirr <lazuardi.rea@gmail.com>
2015-11-01 22:37:59 +0700

    Implement auth feature

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,10 +18,22 @@ Route::resource('agents', 'AgentsController');
 Route::get('movies/json/{title}', 'MoviesController@getMovie');
 Route::resource('movies', 'MoviesController');
 Route::resource('server', 'ServerController');
-//Route::resource('development', 'DevelopmentController');
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+
 Route::get('broadcast', function(){
     return view('pages.broadcastiot');
 });
 Route::get('broadcast/sources', function(){
     return htmlspecialchars(view('pages.broadcastiot')->render());
+});
+Route::get('bc2', function () {
+    return view('pages.bc2');
+});
+Route::get('ieee/iot/2015/pricing', function () {
+    return view('pages.bc2');
 });
