9e46f779f6a19f450659b16d79e9be569649b620
lazuardirr <lazuardi.rea@gmail.com>
2015-11-03 04:49:32 +0700

    Update version number on footer

--- resources/views/pages/partials/adminlte/footer.blade.php
+++ resources/views/pages/partials/adminlte/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <b>Version</b> 0.7.1
+        <b>Version</b> 0.8.1
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2015 <a href="#">Lazuardi R. Rizkina</a>.</strong> All rights reserved.
